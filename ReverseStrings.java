037a48a0cbd4e16620e1c90ed9e7bced1f61f982
Sourya Bhattacharya <souryabhattacharya21@gmail.com>
2024-02-22 17:17:37 +0530

    problem: reverse string - use StringBuilder + char Array

---
+++ ReverseStrings.java
@@ -0,0 +1,18 @@
+public class ReverseStrings {
+    public static void main(String[] args) {
+        String code = "code";
+        System.out.println(code);
+        StringBuilder sb = new StringBuilder(code);
+        String codeReversed = sb.reverse().toString();
+        System.out.println(codeReversed);
+
+        /* using character array */
+        char[] charArray = code.toCharArray();
+        for (int i = 0, mirroredIndex = charArray.length - 1; i < mirroredIndex; i++, mirroredIndex--) {
+            char temp = charArray[i];
+            charArray[i] = charArray[mirroredIndex];
+            charArray[mirroredIndex] = temp;
+        }
+        System.out.println(String.valueOf(charArray));
+    }
+}
