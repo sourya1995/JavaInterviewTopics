07e209c1dfc30b2a4a62c0323cd4b47b9d645af5
Sourya Bhattacharya <souryabhattacharya21@gmail.com>
2024-02-22 12:35:36 +0530

    problem: change case of any character in a given string

---
+++ ChangeCaseOfOneCharacter.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class ChangeCaseOfOneCharacter {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the string");
+        String inputString = input.next();
+        char[] characterArray = inputString.toCharArray();
+        System.out.println("Enter the character");
+        String character = input.next();
+        char characterToChange = character.charAt(0);
+
+        for (int i = 0; i < characterArray.length; i++) {
+            if (characterArray[i] == characterToChange) {
+                if (characterToChange >= 'a' && characterToChange <= 'z') {
+                    characterToChange -= 32;
+                }
+
+                else if (characterToChange >= 'A' && characterToChange <= 'Z') {
+                    characterToChange += 32;
+                }
+                characterArray[i] = characterToChange;
+                break;
+            }
+        }
+
+        inputString = String.valueOf(characterArray);
+        System.out.println(inputString);
+
+    }
+}
