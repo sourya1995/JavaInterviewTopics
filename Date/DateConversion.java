4caba451ed3350f31044d2cc49bf959310268915
Sourya Bhattacharya <souryabhattacharya21@gmail.com>
2024-02-23 23:32:33 +0530

    LocalDate to Date and vice versa

--- Date/DateConversion.java
+++ Date/DateConversion.java
@@ -7,12 +7,18 @@ import java.time.LocalDateTime;
 import java.time.ZoneId;
 
 public class DateConversion {
-    Date date = Date.from(Instant.now());
-    ZoneId defaultZoneId = ZoneId.systemDefault();
+    public static void main(String[] args) {
+        Date date = Date.from(Instant.now());
+        ZoneId defaultZoneId = ZoneId.systemDefault();
+        System.out.println(defaultZoneId); //Asia/Kolkata
 
-    LocalDate localDate = date.toInstant().atZone(defaultZoneId).toLocalDate(); // Date to LocalDate
-    Date localDate2 = Date.from(localDate.atStartOfDay(defaultZoneId).toInstant());
+        LocalDate localDate = date.toInstant().atZone(defaultZoneId).toLocalDate(); // Date to LocalDate
+        Date localDate2 = Date.from(localDate.atStartOfDay(defaultZoneId).toInstant());
 
-    LocalDateTime localDateTime =date.toInstant().atZone(defaultZoneId).toLocalDateTime();
-    Date localDate3 = Date.from(localDateTime.atZone(defaultZoneId).toInstant());
+        LocalDateTime localDateTime = date.toInstant().atZone(defaultZoneId).toLocalDateTime();
+        System.out.println(localDateTime);
+        Date localDate3 = Date.from(localDateTime.atZone(defaultZoneId).toInstant());
+        System.out.println(localDate3);
+    }
+   
 }
