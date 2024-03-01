011db08c292337d76a9f68320c78e4cd0dfc88a3
Sourya Bhattacharya <souryabhattacharya21@gmail.com>
2024-03-01 17:14:25 +0530

    getClass() method

---
+++ OOP/Constructors/SpecificUser.java
@@ -0,0 +1,21 @@
+package OOP.Constructors;
+
+public class SpecificUser extends User {
+    private String specificUserID;
+
+    public SpecificUser(String specificUserID, long userID, String name) {
+        super(userID, name);
+        this.specificUserID = specificUserID;
+    }
+
+    public static void main(String[] args) {
+        User user = new User(879745, "John");
+        SpecificUser specificUser = new SpecificUser("1AAAA", 877777, "Jim");
+        User anotherSpecificUser = new SpecificUser("2BBBB", 877777, "Jim");
+
+        System.out.println(user.getClass());
+        System.out.println(specificUser.getClass());
+        System.out.println(anotherSpecificUser.getClass());
+    }
+
+}
---
+++ OOP/Constructors/User.java
@@ -0,0 +1,11 @@
+package OOP.Constructors;
+
+public class User {
+    private long userID;
+    private String name;
+    public User(long userID, String name) {
+        this.userID = userID;
+        this.name = name;
+    }
+
+}
