703a008bfe8573a407f130708187f2407cef56a3
Sourya Bhattacharya <souryabhattacharya21@gmail.com>
2024-03-03 18:35:36 +0530

    static members vs object members

--- OOP/Members/ObjectMemberVStaticMember.java
+++ OOP/Members/ObjectMemberVStaticMember.java
@@ -12,9 +12,10 @@ public class ObjectMemberVStaticMember {
     public static void main(String[] args) {
         final ObjectMemberVStaticMember o1 = new ObjectMemberVStaticMember();
         final ObjectMemberVStaticMember o2 = new ObjectMemberVStaticMember();
-        o1.increment();
-        o2.increment();
-        o2.increment();
+        o1.increment(); //static counter increments 
+        o2.increment(); //static counter increments
+        o1.increment(); //static counter increments
+        o2.increment(); //static counter increments
 
         System.out.println("o1 static counter " + o1.staticCounter);
         System.out.println("o1 member counter " + o1.memberCounter);
@@ -23,6 +24,7 @@ public class ObjectMemberVStaticMember {
         System.out.println("o2 member counter " + o2.memberCounter);
         System.out.println();
         System.out.println("ObjectMemberVsStaticMember.staticCounter = " +
-                ObjectMemberVStaticMember.staticCounter);
+                ObjectMemberVStaticMember.staticCounter); //here, I am calling the static member on the class - this is the right way to deal
+                                                        // with static members
     }
 }
