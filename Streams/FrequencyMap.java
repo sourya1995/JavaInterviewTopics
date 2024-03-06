e610281850381fea57730dd6598ee8ef74ae5f2a
Sourya Bhattacharya <souryabhattacharya21@gmail.com>
2024-03-06 17:12:10 +0530

    IMPORTANT - Frequency Map

---
+++ Streams/FrequencyMap.java
@@ -0,0 +1,14 @@
+package Streams;
+
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class FrequencyMap {
+    public static void main(String[] args) {
+        Stream.of("apple", "orange", "banana", "apple")
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet()
+                .forEach(System.out::println);
+    }
+}
