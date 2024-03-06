46f8470f125c4776b0b94cfc5e3b6e600459e1d1
Sourya Bhattacharya <souryabhattacharya21@gmail.com>
2024-03-06 17:01:15 +0530

    Creating a reusable iterable of streams

---
+++ Streams/ReusableIterable.java
@@ -0,0 +1,21 @@
+package Streams;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ReusableIterable {
+    public static void main(String[] args) {
+        List<String> list = Arrays.asList("FOO", "BAR");
+        Iterable<String> iterable = () -> list.stream().map(String::toLowerCase).iterator();
+        for (String s : iterable) {
+            System.out.println(s);
+        }
+
+        for(String s : iterable){
+            System.out.println(s);
+        }
+
+        
+    }
+}
