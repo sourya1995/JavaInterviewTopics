0d8f12a5f8d5a560bb94ab21c6d977b7a1dfa32d
Sourya Bhattacharya <souryabhattacharya21@gmail.com>
2024-03-07 19:51:40 +0530

    Optional to List via Streams

---
+++ Streams/OptionalToValues.java
@@ -0,0 +1,20 @@
+package Streams;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class OptionalToValues {
+    public static void main(String[] args) {
+        Optional<String> op1 = Optional.empty();
+        Optional<String> op2 = Optional.of("Hello, World");
+
+        List<String> result = Stream.of(op1, op2)
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .collect(Collectors.toList());
+
+        System.out.println(result);
+    }
+}
