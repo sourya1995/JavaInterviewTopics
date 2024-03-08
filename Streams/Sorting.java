e430b43242c51fce423df709ebcd0fbc42382045
Sourya Bhattacharya <souryabhattacharya21@gmail.com>
2024-03-08 09:31:38 +0530

    Sorting Streams

---
+++ Streams/Sorting.java
@@ -0,0 +1,22 @@
+package Streams;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Sorting {
+    public static void main(String[] args) {
+        List<String> data = new ArrayList<>();
+        data.add("Sydney");
+        data.add("London");
+        data.add("New York");
+        data.add("Amsterdam");
+        data.add("Mumbai");
+        data.add("California");
+        List<String> sortedData = data.stream().sorted((s1, s2) -> s1.compareTo(s2)).collect(Collectors.toList());
+        List<String> reverseData = data.stream().sorted(Comparator.reverseOrder()).collect(Collectors.toList());
+        System.out.println(sortedData);
+
+    }
+}
