a99f41a344be7e7e149ae61194691c4ce7bfc378
Sourya Bhattacharya <souryabhattacharya21@gmail.com>
2024-03-09 13:41:57 +0530

    Java 8 - get All Lines from a file

--- IO/ReadAllLinesOfaFileInList.java
+++ IO/ReadAllLinesOfaFileInList.java
@@ -5,16 +5,25 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ReadAllLinesOfaFileInList {
     public List<String> getAllLines(String fileName) throws IOException {
         List<String> lines = new ArrayList<String>();
         try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
             String line = null;
-            while((line = br.readLine()) != null){
+            while ((line = br.readLine()) != null) {
                 lines.add(line);
             }
         }
         return lines;
+    }
+
+    // Java 8
+    public List<String> getAllLinesWithStreams(String fileName) throws IOException {
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            return br.lines().collect(Collectors.toList());
+            
+        }
     }
 }
