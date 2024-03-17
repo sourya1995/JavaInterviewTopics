da091799f0f541314311784f0f582c0968e9b81a
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2024-03-17 17:08:38 +0530

    writing to files using Files API

--- IO/Paths/WritingToFiles.java
+++ IO/Paths/WritingToFiles.java
@@ -5,13 +5,18 @@ import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.List;
 
-public class ReadingFiles {
-    public static void main(String[] args) throws IOException {
+public class WritingToFiles {
+     public static void main(String[] args) throws IOException {
         Path p2 = Paths.get(URI.create("file:///home/testuser/File.txt"));
         byte[] content = Files.readAllBytes(p2); //read bytes into a byte array
         List<String> linesOfContent = Files.readAllLines(p2); //read lines into a list of strings
+
+        List<String> lines = Arrays.asList(new String[]{"First Line", "Second Line", "Third Line"});
+        Files.write(p2, lines);
+        Files.write(p2, content);
 
     }
 }
