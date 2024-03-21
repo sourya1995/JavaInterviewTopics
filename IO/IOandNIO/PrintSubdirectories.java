fdd2c828c571f9706f0fc690eda0ec98b798fc38
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2024-03-21 13:22:35 +0530

    Iterate through a filesystem and filter various formats

--- IO/IOandNIO/PrintSubdirectories.java
+++ IO/IOandNIO/PrintSubdirectories.java
@@ -2,8 +2,10 @@ package IO.IOandNIO;
 
 import java.io.IOException;
 import java.nio.file.DirectoryStream;
+import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.PathMatcher;
 import java.nio.file.Paths;
 
 public class PrintSubdirectories {
@@ -13,6 +15,16 @@ public class PrintSubdirectories {
             if (Files.isDirectory(path)) {
                 System.out.println(path.getFileName());
                
+            }
+        }
+    }
+
+    public void iterateAndFilter() throws IOException {
+        Path dir = Paths.get("C:/for/foo");
+        PathMatcher matcher = FileSystems.getDefault().getPathMatcher("regex:.*(?i:jpg|jpeg|png|gif|bmp|jpe|jfif)");
+        try (DirectoryStream<Path> stream = Files.newDirectoryStream(dir, entry -> matcher.matches(entry))) {
+            for (Path entry : stream) {
+                System.out.println(entry.getFileName());
             }
         }
     }
