56103a33d43560dc2d6ae7a2f75defb0c00e623d
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2024-04-03 00:24:20 +0530

    AtomicInteger usage

--- Concurrency/Synchronized/SynchronizedExample.java
+++ Concurrency/Synchronized/SynchronizedExample.java
@@ -3,19 +3,23 @@ package Concurrency.Synchronized;
 import java.text.MessageFormat;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class SynchronizedExample {
 
-    private static int t = 0;
+    // private static int t = 0;
     private static Object mutex = new Object();
+    private static AtomicInteger t = new AtomicInteger(0);
 
     public static void main(String[] args) {
         ExecutorService executorService = Executors.newFixedThreadPool(400);
         for (int i = 0; i < 100; i++) {
             executorService.execute(() -> {
-                synchronized (mutex) {
-                    t++;
-                    System.out.println(MessageFormat.format("t: {0}", t));
+                synchronized (mutex) { //if I remove this
+                    // t++; //not atomic
+
+                    int currentT = t.incrementAndGet();
+                    System.out.println(MessageFormat.format("t: {0}", currentT));
                 }
             });
 
