162591676ced12eb38513259594cb13e6505869e
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2024-04-06 10:33:47 +0530

    Synchronized counter

--- Concurrency/AtomicTypes/Counter.java
+++ Concurrency/AtomicTypes/Counter.java
@@ -1,5 +1,30 @@
 package Concurrency.AtomicTypes;
 
 public class Counter {
-    /* will complete tomorrow - cheat day */
+    private final int[] counters;
+
+    public Counter(int numberOfcounters) {
+        counters = new int[numberOfcounters];
+    }
+
+    public synchronized void count(int number){
+        if(number >= 0 && number < counters.length){
+            counters[number]++; //increment the integer at given index
+        }
+    }
+
+    public synchronized int getCount(int number){
+        return (number >= 0 && number < counters.length) ? counters[number] : -1; //return the integer at given index or -1 if invalid index)
+    }
+
+    
 }
+/* The issue with this code is - if there are a large number of threads, there will be a bottleneck - lock contention
+ *  - each synchronized method call will start the current thread acquiring lock for the Counter instance
+ * - it will hold the lock until the number value is checked , then the counter is updated accordingly
+ * - finally the lock is released for others to access
+ * - if one thread attempts to acquire the lock while another thread is holding the lock, the attempting thread will BLOCK
+ * - if multiple threads attempt to acquire the lock, one of them will get it, others will continue to block
+ * - the OS will try to switch to another thread - this has a HUGE performance impact on processor
+ * - if multiple threads are waiting, fairness is not guaranteed - this could lead to starvation
+ */
