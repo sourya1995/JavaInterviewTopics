7528bce98ec4542c0abf8d55ffd4b6c0f01ecff5
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2024-05-24 10:01:09 +0530

    Count occurrences of a character

---
+++ JavaCodingProblems/Basics/CountOccurrencesOfACharacter.java
@@ -0,0 +1,20 @@
+package JavaCodingProblems.Basics;
+
+public class CountOccurrencesOfACharacter {
+    public static int countCertainCharacter(String str, char c) {
+        return str.length() - str.replace(String.valueOf(c) , "").length();
+
+    }
+
+    public static int countCertainCharacter2(String str, char c) {
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == c) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+
+}
