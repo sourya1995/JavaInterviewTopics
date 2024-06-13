41dcc03ee4690583218c08f83acbd1fdec7f6522
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2024-06-13 15:41:46 +0530

    MappedByteBuffer practice

--- JavaCodingProblems/IO/MappedByteBufferExample.java
+++ JavaCodingProblems/IO/MappedByteBufferExample.java
@@ -4,23 +4,27 @@ import java.nio.file.StandardOpenOption;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.util.EnumSet;
 
-
 public class MappedByteBufferExample {
-    try(FileChannel fileChannel = (FileChannel.open(somefile, EnumSet.of(StandardOpenOption.READ)))) {
+    try(
+    FileChannel fileChannel = (FileChannel.open(somefile, EnumSet.of(StandardOpenOption.READ))))
+    {
         MappedByteBuffer buffer = fileChannel.map(FileChannel.MapMode.READ_ONLY, 0, fileChannel.size());
 
-        if(buffer != null){
+        if (buffer != null) {
             String bufferContent = StandardCharsets.UTF_8.decode(buffer).toString();
             System.out.println(bufferContent);
             buffer.clear();
         }
     }
 
-    //For huge files, it is advisable to traverse the buffer with a fixed size, as follows:
-    private static final int MAP_SIZE = 5242880;
-    try(FileChannel fileChannel = (FileChannel.open(somefile, EnumSet.of(StandardOpenOption.READ)))) {
+    // For huge files, it is advisable to traverse the buffer with a fixed size, as
+    // follows:
+    private static final int MAP_SIZE = 5242880;try(
+    FileChannel fileChannel = (FileChannel.open(somefile, EnumSet.of(StandardOpenOption.READ))))
+    {
         int position = 0;
         long length = fileChannel.size();
         while(position < length) {
@@ -35,9 +39,43 @@ public class MappedByteBufferExample {
             position += bytestomap;
 
 
-        }
+    }
+    
+    //binary files
+    try(FileChannel fileChannel = (FileChannel.open(binaryFile, EnumSet.of(StandardOpenOption.READ)))) {
+        MappedByteBuffer buffer = fileChannel.map(FileChannel.MapMode.READ_ONLY, 0, fileChannel.size());
+        System.out.println(buffer.limit() + "read");
+
+    }
+
+    private static final int MAP_SIZE = 5242880;try(
+    FileChannel channel = FileChannel.open(binaryFile, EnumSet.of(StandardOpenOption.READ))){
+        int position = 0;
+        long length = channel.size();
+        while(position < length){
+            long remaining = length - position;
+            int bytestomap = (int) Math.min(MAP_SIZE, remaining);
+            try {
+                MappedByteBuffer buffer = fileChannel.map(FileChannel.MapMode.READ_ONLY, position, bytestomap);
+               =
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            position += bytestomap;
+    }
+       
 
 
 
+    
 
+    //writing binary files
+    try(FileChannel fileChannel = Files.newByteChannel(path, EnumSet.of(StandardOpenOption.CREATE, StandardOpenOption.READ, StandardOpenOption.WRITE))) {
+        MappedByteBuffer buffer = fileChannel.map(FileChannel.MapMode.READ_WRITE, 0, length);
+
+        if(buffer != null) {
+            buffer.put(data)
+
+    }
 }
