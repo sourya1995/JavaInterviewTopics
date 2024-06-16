a97894d0397f1673cf7480c24c9b4bc9ffa62afd
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2024-06-16 16:54:07 +0530

    parentheses fix

--- JavaCodingProblems/FunctionalStyleProgramming/ObserverPattern/Main.java
+++ JavaCodingProblems/FunctionalStyleProgramming/ObserverPattern/Main.java
@@ -1,20 +1,27 @@
 package JavaCodingProblems.FunctionalStyleProgramming.ObserverPattern;
 
 public class Main {
-   public static void main(String[] args) {
-    FireStation firestation = new FireStation();
-    firestation.registerFireStation(new BrookhavenFireStation());
-    firestation.registerFireStation(new ViningsFireStation());
 
-    firestation.registerFireStation((String adderss) ->{
-        if(adderss.contains("Brookhaven")){
-            System.out.println("Brookhaven Fire Station");
-        }else{
-            System.out.println("Vinings Fire Station");
-        }
-    });
-   }
+    public static void main(String[] args) {
+        FireStation firestation = new FireStation();
+        firestation.registerFireStation(new BrookhavenFireStation());
+        firestation.registerFireStation(new ViningsFireStation());
 
+        firestation.registerFireStation((String adderss) -> {
+            if (adderss.contains("Brookhaven")) {
+                System.out.println("Brookhaven Fire Station");
+            } else {
+                System.out.println("Vinings Fire Station");
+            }
+        });
 
+        firestation.registerFireStation((String address) -> {
+            if (address.contains("Vinings")) {
+                System.out.println("Vinings Fire Station");
+            } else {
+                System.out.println("Brookhaven Fire Station");
+            }
+        });
 
+    }
 }
