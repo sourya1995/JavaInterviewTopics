147cee195fd0ffb246bbea89d5186c11683d8feb
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2024-06-17 11:35:29 +0530

    Filter non-zero values

---
+++ JavaCodingProblems/DeepDiveFunctional/StreamOps.java
@@ -0,0 +1,35 @@
+package JavaCodingProblems.DeepDiveFunctional;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+public class StreamOps {
+    public static void main(String[] args) {
+        List<Integer> ints = Arrays.asList(1, 2, -4, 0, 2, 0, -1, 14, 0, -1);
+        List<Integer> result = ints
+        .stream()
+        .peek(s -> System.out.println("\tstream():" + s))
+        .filter(i -> i != 0)
+        .peek(s -> System.out.println("\tfilter():" + s))
+        .collect(Collectors.toList());
+        System.out.println(result);
+
+        List<Integer> result2 = ints
+        .stream()
+        .peek(s -> System.out.println("\tstream():" + s))
+        .filter(i -> i != 0)
+        .peek(s -> System.out.println("\tfilter():" + s))
+        .distinct()
+        .peek(s -> System.out.println("\tdistinct():" + s))
+        .skip(1)
+        .peek(s -> System.out.println("\tskip():" + s))
+        .limit(3)
+        .peek(s -> System.out.println("\tlimit():" + s))
+        .collect(Collectors.toList());
+        System.out.println(result2);
+
+
+    }
+}
