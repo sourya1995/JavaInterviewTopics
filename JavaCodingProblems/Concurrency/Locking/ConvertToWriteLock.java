f51149f8fed6a75ba35212c7c88a4068c4fcaf3a
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2024-06-26 09:05:55 +0530

    Convert to WRITE LOCK

---
+++ JavaCodingProblems/Concurrency/Locking/ConvertToWriteLock.java
@@ -0,0 +1,28 @@
+package JavaCodingProblems.Concurrency.Locking;
+
+import java.util.concurrent.locks.StampedLock;
+
+public class ConvertToWriteLock {
+	private final StampedLock lock = new StampedLock();
+	private int balance = 1000;
+	public void withdraw(int amount) {
+		long stamp = lock.readLock();
+		try {
+			while(amount <= balance) {
+				long convertStamp = lock.tryConvertToWriteLock(stamp);
+				if(convertStamp != 0L) {
+					System.out.println("lock successfully converted");
+					stamp = convertStamp;
+					balance = balance - amount;
+					System.out.println("New balance = " + balance);
+					break;
+				} else {
+					lock.unlockRead(stamp);
+					stamp = lock.writeLock();
+				}
+			}
+		} finally {
+			lock.unlock(stamp);
+		}
+	}
+}
